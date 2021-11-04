37844f9434a2777af713e9d6e6b28d60d004b45f
makeit-mike <mike.isaac.jones@gmail.com>
2021-11-03 21:09:03 -0500

    Initial Commit

---
+++ CoffeeCalc/CoffeeCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeCalcApp.swift
+//  CoffeeCalc
+//
+//  Created by Michael Jones on 11/3/21.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView(rGuess: 0.5, gGuess: 0.5, bGuess: 0.5)
+        }
+    }
+}
