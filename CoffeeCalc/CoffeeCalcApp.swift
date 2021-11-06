e69297abb670edb5bb902d6eda4289fa948a9685
makeit-mike <mike.isaac.jones@gmail.com>
2021-11-06 17:18:45 -0500

    Fixing many UI issues. Better buttons. Invest form added

--- CoffeeCalc/CoffeeCalcApp.swift
+++ CoffeeCalc/CoffeeCalcApp.swift
@@ -12,7 +12,7 @@ struct CoffeeCalcApp: App {
     var body: some Scene {
         WindowGroup {
             // ColorPickerView(rGuess: 0.5, gGuess: 0.5, bGuess: 0.5) // Color Picker Demo
-            ContentView()
+            StocksView()
         }
     }
 }
--- CoffeeCalc/StocksView.swift
+++ CoffeeCalc/StocksView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Combine
 
 struct StocksView: View {
     var body: some View{
@@ -13,7 +14,7 @@ struct StocksView: View {
             HeaderView()
             BodyView()
             FooterView()
-        }.padding(10).background(.black).foregroundColor(.white).preferredColorScheme(.dark)
+        }.padding(10).background(Color(red: 1.0, green: 1.0, blue: 1.0, opacity: 0.145)).foregroundColor(.white).preferredColorScheme(.dark)
     }
 }
 
@@ -21,12 +22,12 @@ struct HeaderView: View {
     var body: some View{
         VStack{
             Text("Investment Quest").fontWeight(.semibold)
-                .font(.title).padding(-1)
+                .font(.title).padding(10)
         }.frame(
             minWidth: 0,
             maxWidth: .infinity,
             minHeight: 0,
-            maxHeight: 48,
+            maxHeight: 90,
             alignment: .topLeading
         )
     }
@@ -35,12 +36,13 @@ struct HeaderView: View {
 struct BodyView: View {
     var body: some View{
         VStack{
-            StockSearchView().frame(alignment: .center)
+            StockSearchView().frame(alignment: .center).padding(20)
         }.frame(
             minWidth: 0,
             maxWidth: .infinity,
             minHeight: 0,
-            maxHeight: 700
+            maxHeight: 700,
+            alignment: .topLeading
         ).padding(-10)
     }
 }
@@ -51,6 +53,7 @@ struct FooterView: View {
             HStack{
                 FooterNavButtonView(iconName: "magazine", btnText: "Portfolio")
                 FooterNavButtonView(iconName: "magnifyingglass", btnText: "Search")
+                FooterNavButtonView(iconName: "gear", btnText: "Settings")
             }.font(.title2)
         }.frame(
             minWidth: 0,
@@ -68,47 +71,128 @@ struct FooterNavButtonView: View {
     
     var body: some View{
         Button(action: {}) {
-            HStack {
+            VStack {
                 Image(systemName: iconName)
-                    .font(.title)
+                    .font(.title).frame(width: 35, height: 35)
                 Text(btnText)
-                    .fontWeight(.semibold)
-                    .font(.title)
-            }
-            .padding(10)
-            .foregroundColor(.white)
-            .background(Color.gray)
-            .cornerRadius(20)
+                    .fontWeight(.light)
+                    .font(.caption)
+            }.frame(width: 50, height: 50, alignment: .top)
+                .padding(10)
+                .foregroundColor(.white)
+                .background(Color.gray)
+                .cornerRadius(20)
         }
     }
 }
 
 struct StockSearchView: View {
     @State private var stockSymbol: String = ""
+    @State private var showStockInfo: Bool = false
+    @State private var showInvestView: Bool = false
+    @State private var stockPrice: Double = Double.random(in: 20 ..< 1000)
+    @State private var stockGrowth: Double = Double.random(in: 0 ..< 1)
+    @State private var stockShareHolders: Int = Int.random(in: 50 ..< 10000000)
     var body: some View{
         VStack{
-            /*
-             Input "Enter Stock Symbol"
-             Input Email
-             Input PWord
-             Input Addr
-             Input City
-             Input State
-             Click Invest
-             Click Cancel
-             */
             HStack{
                 Text("Stock Symbol:")
-                TextField("", text: $stockSymbol).foregroundColor(.white).background(.gray).padding(0).cornerRadius(20).frame(width: 120, height: .infinity, alignment: .topLeading).font(.title).opacity(0.5)
+                TextField("", text: $stockSymbol)
+                    .foregroundColor(.white)
+                    .background(.gray)
+                    .cornerRadius(20)
+                    .frame(width: 120, alignment: .topLeading)
+                    .font(.title).opacity(0.5)
+                    .onReceive(Just($stockSymbol)) { _ in limitStockSymbol(5) }
+                
+                Button(action: {showStockInfo = true}) {
+                    Image(systemName: "magnifyingglass")
+                }.padding(10)
+                    .foregroundColor(.white)
+                    .background(Color.gray)
+                    .cornerRadius(20)
+            }.frame(width: 300, height: 60)
+            
+            if(showStockInfo){
+                Text("Stock information for \(stockSymbol)").foregroundColor(.white).frame(width: 300, height: 20, alignment: .topLeading)
+                Text("Stock Price: $\(stockPrice)").foregroundColor(.white).frame(width: 300, height: 20, alignment: .topLeading)
+                Text("Estimated Growth: \(stockGrowth)%").foregroundColor(.white).frame(width: 300, height: 20, alignment: .topLeading)
+                Text("Shareholders: \(stockShareHolders)").foregroundColor(.white).frame(width: 300, height: 20, alignment: .topLeading)
+                
+                Button(action: {self.showInvestView.toggle()}) {
+                    HStack{
+                        Image(systemName: "dollarsign.square.fill")
+                        Text("Invest")
+                    }
+                }.frame(width: 80, alignment: .topLeading).padding(10)
+                    .foregroundColor(.white)
+                    .background(Color.gray)
+                    .cornerRadius(20)
+                    .sheet(isPresented: $showInvestView) {
+                        InvestView()
+                    }
             }
-            
-            Text("Stock Price: $123.45").foregroundColor(.white).frame(width: 300, height: 20, alignment: .topLeading)
-            Text("Estimated Growth: 0.025%").foregroundColor(.white).frame(width: 300, height: 20, alignment: .topLeading)
-            Text("Shareholders: 2.5 mil").foregroundColor(.white).frame(width: 300, height: 20, alignment: .topLeading)
-            
-            
         }.frame(width: 300, height: 300, alignment: .topLeading)
         
+    }
+    func limitStockSymbol(_ upper: Int) {
+        stockSymbol = String(stockSymbol.prefix(upper)).uppercased()
+    }
+}
+
+struct InvestView: View {
+    @State private var email: String = ""
+    @State private var fullName: String = ""
+    @State private var dateOfBirth: Date = Date.now
+    @State private var address: String = ""
+    @State private var phoneNumber: String = ""
+    
+    @State var stockSymbol: String = ""
+    @State var showStockInfo: Bool = false
+    @State var showInvestView: Bool = false
+    @State var stockPrice: Double = 0
+    
+    var body: some View{
+        VStack{
+            Text("Invest in me senpai")
+            UserInfoTextInput(viewResponse: "Full Name:", text: fullName)
+            UserInfoTextInput(viewResponse: "Email Address:", text: email)
+            UserInfoTextInput(viewResponse: "Home Address:", text: address)
+            UserInfoTextInput(viewResponse: "Phone Number:", text: phoneNumber)
+            
+            HStack{
+                Text("Date of Birth:").frame(width: 200)
+                DatePicker(
+                    "Start Date",
+                    selection: $dateOfBirth,
+                    displayedComponents: [.date]
+                ).frame(width: 120, alignment: .topLeading)
+            }
+            
+            HStack{
+                //Num shares use var
+            }
+            HStack{
+                // total cost = stockCost * numShares
+            }
+        }
+    }
+}
+
+struct UserInfoTextInput: View {
+    @State var viewResponse: String = ""
+    @State var text: String = ""
+    var body: some View{
+        HStack{
+            Text(viewResponse).frame(width: 200)
+            TextField("", text: $text)
+                .foregroundColor(.white)
+                .background(.gray)
+                .padding(10)
+                .cornerRadius(40)
+                .frame(width: 120, alignment: .topLeading)
+                .font(.title).opacity(0.5)
+        }
     }
 }
 
